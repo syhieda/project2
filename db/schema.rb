a21f6cf53c36b7e90f5383f9c2ea473383845063
website <syahidahaulia71@gmail.com>
2024-01-25 09:12:35 +0700

    layout done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_24_143337) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_25_014033) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
